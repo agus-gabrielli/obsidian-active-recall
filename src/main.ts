f5b51fd0469f82a670828ec3a2a553acdc802f75
Agustin Gabrielli <agabrielli@fi.uba.ar>
2026-03-11 20:29:05 -0300

    feat(04-03): wire sidebar view, CMD-02, CMD-03, ribbon into main.ts
    - Register ActiveRecallSidebarView with registerView
    - Add CMD-02: Open Active Recall Panel (command palette)
    - Register file-menu event for CMD-03 context menu on folders
    - Add ribbon icon (brain-circuit) that opens/focuses panel
    - Add detachLeavesOfType in onunload to prevent duplicate panes

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { Notice, Plugin } from 'obsidian';
 import { ActiveRecallSettings, DEFAULT_SETTINGS, ActiveRecallSettingTab } from './settings';
 import { GenerationService } from './generation';
+import { VIEW_TYPE_ACTIVE_RECALL, ActiveRecallSidebarView, buildActivateView, buildContextMenuHandler } from './sidebar';
 
 export default class ActiveRecallPlugin extends Plugin {
     settings: ActiveRecallSettings;
@@ -12,6 +13,30 @@ export default class ActiveRecallPlugin extends Plugin {
         const statusBarItem = this.addStatusBarItem();
         statusBarItem.setText('');
         const generationService = new GenerationService(this.app, this.settings, statusBarItem);
+
+        this.registerView(
+            VIEW_TYPE_ACTIVE_RECALL,
+            (leaf) => new ActiveRecallSidebarView(leaf, this.app, generationService)
+        );
+
+        const activateView = buildActivateView(this.app);
+
+        this.addCommand({
+            id: 'open-active-recall-panel',
+            name: 'Open Active Recall Panel',
+            callback: () => activateView(),
+        });
+
+        this.registerEvent(
+            this.app.workspace.on(
+                'file-menu',
+                buildContextMenuHandler(generationService.generate.bind(generationService), this.app, VIEW_TYPE_ACTIVE_RECALL)
+            )
+        );
+
+        this.addRibbonIcon('brain-circuit', 'Open Active Recall Panel', () => {
+            activateView();
+        });
 
         this.addCommand({
             id: 'generate-self-test',
@@ -28,7 +53,9 @@ export default class ActiveRecallPlugin extends Plugin {
         });
     }
 
-    onunload() {}
+    onunload() {
+        this.app.workspace.detachLeavesOfType(VIEW_TYPE_ACTIVE_RECALL);
+    }
 
     async loadSettings() {
         this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData() as Partial<ActiveRecallSettings>);
